8393b03ab1cf9f03fe75974f5f25bde024cf8c84
Chico Sokol <chico.sokol@gmail.com>
2012-07-10 19:44:42 -0300

    fixing tests

--- test/PygmentsCacheTest.py
+++ test/PygmentsCacheTest.py
@@ -1,23 +1,23 @@
 import unittest
-from cache import *
+from pygments_cache.cache import *
 
 class PygmentsCacheTest(unittest.TestCase): 
 
     def test_verify_input_file_argument(self):
-        cache = PygmentizeCache(['pygmentize', '-o', 'output', 'input.html'], PygmentizeExecutor())
+        cache = PygmentizeCache(['pygmentize', '-o', 'output', 'input.html'], PygmentizeExecutor(CacheManipulator()), CacheManipulator())
         self.assertTrue(cache.contains_input_file())
         
     def test_verify_stdin_argument(self):
-        cache = PygmentizeCache(['pygmentize', '-o', 'output'], PygmentizeExecutor())
+        cache = PygmentizeCache(['pygmentize', '-o', 'output'], PygmentizeExecutor(CacheManipulator()), CacheManipulator())
         self.assertFalse(cache.contains_input_file())
         
     def test_verify_pygmentize_args_parse(self):
-        cache = PygmentizeCache(['pygmentize', '-f', 'latex', '-o', 'output', 'index.html'], PygmentizeExecutor())
+        cache = PygmentizeCache(['pygmentize', '-f', 'latex', '-o', 'output', 'index.html'], PygmentizeExecutor(CacheManipulator()), CacheManipulator())
         pygmentize_cmd = cache.parse_pygmentizeargs()
         self.assertEqual(pygmentize_cmd, 'pygmentize -f latex -o output index.html')
         
     def test_find_output_filename(self):
-        cache = PygmentizeCache(['pygmentize', '-f', 'latex', '-o', 'output', 'index.html'], PygmentizeExecutor())
+        cache = PygmentizeCache(['pygmentize', '-f', 'latex', '-o', 'output', 'index.html'], PygmentizeExecutor(CacheManipulator()), CacheManipulator())
         outputfilename = cache.find_output_filename()
         self.assertEqual(outputfilename, 'output')
         
