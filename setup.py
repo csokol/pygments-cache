aca6349f4efec731789808e992af9059dd39dbf4
Chico Sokol <chico.sokol@gmail.com>
2012-07-31 15:24:34 -0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pygments_cache',
+    version='0.1',
+    description='',
+    author='Caelum',
+    author_email='francisco.sokol@caelum.com.br',
+    url='https://github.com/csokol/pygments-cache',
+    packages=['pygments_cache'],
+      long_description="",
+      classifiers=[
+          "License ::",
+          "Programming Language :: Python",
+          "Development Status :: ",
+      ],
+      keywords='',
+      license='',
+      install_requires=[
+        'setuptools',
+        'pygments',
+      ],
+      scripts=['pygmentize'],
+)
