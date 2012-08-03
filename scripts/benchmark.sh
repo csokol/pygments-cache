812813e1d672f65c56a615e03abf63718dfce0b5
Chico Sokol <chico.sokol@gmail.com>
2012-08-03 15:20:14 -0300

    imporving benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-PYGMENTIZE_CACHED_PATH=pygmentize-cached
+declare -a PYGMENTS_TO_TEST
 
-run_original() {
+#PYGMENTS_TO_TEST[0]=/home/csokol/caelum/pygments-cache/pygmentize-cached
+PYGMENTS_TO_TEST[0]=/usr/bin/pygmentize
+PYGMENTS_TO_TEST[1]=/usr/bin/pygmentize-cached
+#PYGMENTIZE_CACHED_PATH=pygmentize-cached
+
+run_a_pygmentize() {
     for file in $(ls examples); do
         #echo $file
-        pygmentize -o ~/tmp/$file.html -f html examples/$file
-    done
-}
-run_cached() {
-    for file in $(ls examples); do
-        #echo $file
-        $PYGMENTIZE_CACHED_PATH -o ~/tmp/$file.html -f html examples/$file
+        $PYGMENTIZE_TO_RUN -o ~/tmp/$file.html -f html examples/$file
     done
 }
 
 echo "starting benchmark for $(ls examples | wc -l) files..."
 echo ""
 
-echo "original pygmentize timings: "
-time run_original
-echo ""
-
-echo "pygmentize-cached timings: "
-time run_cached
+for path in "${PYGMENTS_TO_TEST[@]}"; do
+    PYGMENTIZE_TO_RUN=$path
+    echo "timings for: $path"
+    time run_a_pygmentize
+    echo ""
+done
